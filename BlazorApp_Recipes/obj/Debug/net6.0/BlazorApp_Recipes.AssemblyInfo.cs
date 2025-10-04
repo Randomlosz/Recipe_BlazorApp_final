d219b2dfeadcd0a2ecf18bc8e43d12aea5967066
Randomlosz <jankura.ren@gmail.com>
2025-10-04 19:24:43 +0200

    new function: details page
    new page for the recipe's detailed view, here a function that recalculate the ingredients by servings

--- BlazorApp_Recipes/obj/Debug/net6.0/BlazorApp_Recipes.AssemblyInfo.cs
+++ BlazorApp_Recipes/obj/Debug/net6.0/BlazorApp_Recipes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp_Recipes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcee6cbe381d05bd6809d8004fdefaf3a71bdd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd8425224ee45cb2ddfaef841d00a97e70caa66")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp_Recipes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp_Recipes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
